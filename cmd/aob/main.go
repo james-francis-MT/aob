e0b78d9fc87634589021c171674f74c080a7dcd5
james-francis-mt <james.francis.jef@gmail.com>
2025-11-28 21:01:57 +0000

    dont gitignore main.go

---
+++ cmd/aob/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/James-Francis-MT/aob/internal/advent"
+	"github.com/James-Francis-MT/aob/internal/server"
+)
+
+func main() {
+	// Create the advent calendar for current year
+	currentYear := time.Now().Year()
+	calendar := advent.NewCalendar(currentYear, "content")
+
+	// Create the server
+	srv := server.New(calendar, "templates", "static")
+
+	// Start the HTTP server
+	addr := ":8080"
+	fmt.Printf("Starting Advent of Beckie server on %s\n", addr)
+	fmt.Printf("Visit http://localhost%s\n", addr)
+
+	if err := http.ListenAndServe(addr, srv); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
